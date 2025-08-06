a941b63d9c0b953e366502e75a65ce04b4be9478
Peacemaker <devpeacemd@gmail.com>
2025-08-06 16:16:12 +0300

    ᴘᴇᴀᴄᴇ ʜᴜʙ

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 (function(){
   const _0x3f91 = [
-    '\x2e\x2f\x70\x65\x61\x63\x65\x6d\x61\x6b\x65\x72\x2f\x69\x6e\x64\x65\x78',
-    'require'
+    '\x2e\x2f\x70\x65\x61\x63\x65\x6d\x61\x6b\x65\x72\x2f\x69\x6e\x64\x65\x78'
   ];
-  const _0x4c6b = function(_0x2b17f0, _0x3d96e4) {
-    _0x2b17f0 = _0x2b17f0 - 0x0;
+  const _0x4c6b = function(_0x2b17f0) {
     return _0x3f91[_0x2b17f0];
   };
-  const _0x38a4 = _0x4c6b;
-  global[_0x38a4('1')](_0x38a4('0'));
+  const _0xreq = require;
+  _0xreq(_0x4c6b(0));
 })();
